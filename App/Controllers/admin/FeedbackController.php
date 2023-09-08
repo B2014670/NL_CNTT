8182642f7255cd1b8b35064392fd4b5ec6e1f7cc
luan.nguyen <luanga316@gmail.com>
2023-09-08 16:08:22 +0700

    admin page with end point /admin

---
+++ App/Controllers/admin/FeedbackController.php
@@ -0,0 +1,24 @@
+<?php
+    use App\Core\Controller;
+
+    class FeedbackController extends Controller{
+        private $feedbackModel;
+        function __construct(){
+            $this->feedbackModel = $this->model("FeedbacksModel");
+        }
+
+        function Index(){
+            $data["feedback"] = $this->feedbackModel->showAll();
+            $this->view("feedbacks/index", $data);
+        }
+
+        function delete(){
+            if(isset($_GET)){
+                $result =  $this->feedbackModel->deleteCmt($_GET);
+                echo $result;
+                return;
+            }
+            else echo "Can not delete this comment!"; 
+        }
+    }
+?>
